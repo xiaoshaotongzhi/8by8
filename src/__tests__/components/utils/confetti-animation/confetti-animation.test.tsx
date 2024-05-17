b95ca8b26ca32d18e4db8bdb199ce72604642459
JyyHuang <jhuang6547@gmail.com>
2024-05-16 20:40:35 -0400

    added jest-canvas-mock

--- src/__tests__/components/utils/confetti-animation/confetti-animation.test.tsx
+++ src/__tests__/components/utils/confetti-animation/confetti-animation.test.tsx
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom';
-import { render, cleanup, screen, act } from '@testing-library/react';
+import { render, cleanup, screen } from '@testing-library/react';
 import { ConfettiAnimation } from '@/components/utils/confetti-animation';
+import { act } from 'react-dom/test-utils'
 
 describe('ConfettiAnimation', () => {
   afterEach(cleanup);
